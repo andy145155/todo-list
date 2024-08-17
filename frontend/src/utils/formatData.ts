bd7991789e3bd61e948d0417263595f45b52684a
andy145155 <andy57291@gmail.com>
2024-08-17 09:49:34 +0000

    Add formatDate utility function and duty schema to frontend code

---
+++ frontend/src/utils/formatData.ts
@@ -0,0 +1,11 @@
+export const formatDate = (date: Date): string => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+
+  return date.toLocaleDateString(undefined, options);
+};
